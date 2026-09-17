e338e00e6573ea5d7daa92bd0160de3db79f7e2e
pdsimari <pdsimari@gmail.com>
2026-09-17 14:11:31 -0400

    added hw3 (not yet linked from main site)

--- _site/questions/day04.c
+++ _site/questions/day04.c
@@ -2,18 +2,35 @@
 
 int main(void) {
 
-  for (int i = 100; i > 0; i -= 20) printf("%d ", i);
+  // Question 6
+  for (int i = 100; i > 0; i -= 20)
+  switch (i % 3) {
+    case 1: printf("%d ", i);
+    case 2: printf("== %2d\n", i); break;
+    default: printf("%03d ", i); break;
+  }
+
+  // generates warning about case 1 fall-through statement
+
+  // output:
+  // 100 == 100
+  // == 80
+  // 060 40 == 40
+  // == 20
+
   //  printf("%d\n", i);  // compiler error
+
+  // Question 9
+  int num = 6;
+  if ( num = 24 / 3 && 'c' > 'D' || ++num) printf("first");
+  else printf("second");
+
+  // generates warning about use of assignment w/o parentheses
   
+  // output: first
 
-int num	= 6;
-
-// generates warning about use of assignment w/o parentheses
-if ( num = 24 / 3 && 'c' > 'D' || ++num)
-   printf("first");
-else
-   printf("second");
- printf("\n %d ", num);
+  printf("\n%d\n", num); // output: 1
   
   return 0;
+
 }
